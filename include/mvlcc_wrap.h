c06677053f69776c7aa1c5e956ea80036022169e
Florian Lüke <f.lueke@mesytec.com>
2024-10-09 16:44:26 +0200

    add mvlcc_vme_block_read()

--- include/mvlcc_wrap.h
+++ include/mvlcc_wrap.h
@@ -41,6 +41,16 @@ const char *mvlcc_strerror(int errnum);
 int mvlcc_is_mvlc_valid(mvlcc_t a_mvlc);
 int mvlcc_is_ethernet(mvlcc_t a_mvlc);
 
+struct MvlccBlockReadParams
+{
+    uint8_t amod; /* amod, must be a valid VME block amod */
+    int fifo: 1;  /* if true the read address is not incremented */
+    int swap: 1;  /* if true swaps the two 32-bit words for 64-bit MBLT reads */
+};
+
+/* bufferInSize and bufferOutSize are in units of 32-bit words */
+int mvlcc_vme_block_read(mvlcc_t a_mvlc, uint32_t address, uint32_t *buffer, size_t bufferInSize,
+  size_t *bufferOutSize, struct MvlccBlockReadParams params);
 
 /* spdlog level names: error, warn, info, debug, trace */
 void mvlcc_set_global_log_level(const char *levelName);
